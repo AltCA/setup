55ed0ccefd41425d64240851519a83e522944a26
Rasmus Sten <rasmus@dll.nu>
2012-08-13 09:00:45 +0300

    clarify output a little

--- setup.sh
+++ setup.sh
@@ -8,22 +8,26 @@ update_cert_if_changed() {
     url="$1"
     command="$2"
     pem=`mktemp -t altca-setup`
-    echo "Using pem file: $pem" 
     curl -sfo $pem "$url"
     commonName=`openssl asn1parse -in $pem -i|grep -1 ':commonName'|grep UTF8STRING|cut -f 4 -d :|uniq|tail -1`
     newSha=`openssl x509 -in $pem -sha1 -noout -fingerprint | cut -f 2 -d = | sed -e 's/://g'`
     echo "Downloaded certificate \"$commonName\" with fingerprint $newSha"
     oldSha=`sha_for "$commonName"`
-    if [ $oldSha ] ; then
-	echo "Found \"$commonName\" with fingerprint $oldSha"
-    fi
+    added=false
+    replaced=false
     if [ "$oldSha" != "$newSha" ] ; then
 	security $command $pem
+	added=true
 	if [ "$oldSha" ] ; then
 	    rmcommand=`echo "$command" | sed -e 's/add-/remove-/'`
-	    echo "Deleting old certificate with fingerpring $oldSha"
 	    security $rmcommand "$oldSha"
+	    replaced=true
 	fi
+    fi
+    if $replaced ; then
+	echo "Replaced certificate \"$commonName\" (old fingerprint: $oldSha, new fingerprint: $newSha)"
+    elif $added ; then
+	echo "Added new certificate \"$commonName\" with fingerprint $newSha"
     fi
 }
 
