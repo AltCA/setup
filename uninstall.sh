7f58fb538334705ffacfe3c31fb20f194bc8f826
Rasmus Sten <rasmus@dll.nu>
2012-08-19 14:06:00 +0300

    uninstall script that should remove all traces of AltCA certs from your system

---
+++ uninstall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sha_for() {
+    security find-certificate -Zc "$1"|grep ^SHA-1|awk '{print $3}'
+}
+
+rootsha=`sha_for "AltCA root"`
+pkgcertsha=`sha_for "AltCA package root"`
+codecertsha=`sha_for "AltCA code signing root"`
+
+rootpem=`mktemp -t altca-uninstall`
+security find-certificate -a -c 'AltCA root' -p > $rootpem
+
+echo "Removing AltCA.org rules from the Gatekeeper security policy"
+sudo spctl --remove --label "AltCA.org root"
+echo "Removing certificates"
+
+security remove-trusted-cert "$rootpem"
+security delete-certificate -Z "$pkgcertsha"
+security delete-certificate -Z "$codecertsha"
+security delete-certificate -Z "$rootsha"
+
+echo "All done."
